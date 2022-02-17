b2a6ae731dda9395e65f67f3f03a42a753d49e95
Sebastian cataño <sebastian.catano@upb.edu.co>
2022-02-17 11:19:07 -0500

    Lectura 2

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void funcPrint(char *pstring){
+    printf("%s\n",pstring);
+}
+
 int main(void){
-    printf("Sistemas Computacionales\n");
-    return EXIT_SUCCESS;   
+    //printf("Sistemas Computacionales\n");
+    //return EXIT_SUCCESS;
+    funcPrint("webo");
+    return(0);
+
 }
